1bdf6112e42d1aed191ae2411f0f67b5e39cba9d
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2025-04-30 00:54:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ LOGIN_TEMPLATE = '''
         
         body {
             font-family: 'Poppins', sans-serif;
-            background-image: url('https://i.postimg.cc/brTPhkYy/20240902-191634.jpg');
+            background-image: url('https://i.postimg.cc/0jCy5rdf/20240831-222518.jpg');
             background-size: cover;
             background-repeat: no-repeat;
             display: flex;
